65642b8dc095799552e59acea5e1179bba7a4662
shimniok@gmail.com <shimniok@gmail.com>
2021-01-10 09:19:19 -0700

    Separate encoder resolution for abs & inc types; added method to get track count; draw outer circle.

--- WheelEncoderGenerator/src/main/java/com/botthoughts/wheelencodergenerator/Encoder.java
+++ WheelEncoderGenerator/src/main/java/com/botthoughts/wheelencodergenerator/Encoder.java
@@ -25,30 +25,59 @@ import javafx.beans.property.SimpleStringProperty;
  *
  * @author mes
  */
-final public class Encoder {
+final public class Encoder implements java.io.Serializable {
 
+    private static String ABSOLUTE = "absolute";
+    private static String INCREMENTAL = "incremental";
+    private static String MM = "mm";
+    private static String INCH = "inch";
+    private static String GRAY = "gray";
+    private static String BINARY = "binary";
+
+    public boolean checkDiameters() {
+        Integer i = this.innerDiameter.getValue();
+        Integer o = this.outerDiameter.getValue();
+        Integer c = this.centerDiameter.getValue();
+        
+        if (c > i || c > o || i > o) {
+            return false;
+        }
+        
+        return true;
+    }
+    
     public SimpleStringProperty getType() {
         return type;
     }
 
     public void setType(SimpleStringProperty type) {
-        this.type = type;
+        if (this.type.equals(this.ABSOLUTE) || this.type.equals(this.INCREMENTAL)) {
+            this.type = type;
+        }
     }
 
-    public SimpleIntegerProperty getResolution() {
-        return resolution;
+    public SimpleIntegerProperty getAbsoluteResolution() {
+        return absoluteResolution;
     }
 
-    public void setResolution(SimpleIntegerProperty resolution) {
-        this.resolution = resolution;
+    public void setAbsoluteResolution(SimpleIntegerProperty absoluteResolution) {
+        if (absoluteResolution.getValue() >= 0) this.absoluteResolution = absoluteResolution;
     }
 
+    public SimpleIntegerProperty getIncrementalResolution() {
+        return incrementalResolution;
+    }
+
+    public void setIncrementalResolution(SimpleIntegerProperty incrementalResolution) {
+        if (incrementalResolution.getValue() >= 2) this.incrementalResolution = incrementalResolution;
+    }
+   
     public SimpleIntegerProperty getOuterDiameter() {
         return outerDiameter;
     }
 
     public void setOuterDiameter(SimpleIntegerProperty outerDiameter) {
-        this.outerDiameter = outerDiameter;
+        if (outerDiameter.getValue() >= 0) this.outerDiameter = outerDiameter;
     }
 
     public SimpleIntegerProperty getInnerDiameter() {
@@ -56,7 +85,7 @@ final public class Encoder {
     }
 
     public void setInnerDiameter(SimpleIntegerProperty innerDiameter) {
-        this.innerDiameter = innerDiameter;
+        if (innerDiameter.getValue() >= 0) this.innerDiameter = innerDiameter;
     }
 
     public SimpleIntegerProperty getCenterDiameter() {
@@ -64,7 +93,7 @@ final public class Encoder {
     }
 
     public void setCenterDiameter(SimpleIntegerProperty centerDiameter) {
-        this.centerDiameter = centerDiameter;
+        if (centerDiameter.getValue() >= 0) this.centerDiameter = centerDiameter;
     }
 
     public SimpleBooleanProperty getInverted() {
@@ -118,9 +147,32 @@ final public class Encoder {
     public List<String> getCodingOptions() {
         return codingOptions;
     }
-   
+    
+    /**
+     * Returns the number of tracks in the encoder, based on its type and various options.
+     * This method is helpful for rendering the encoder. Absolute encoders have a track count
+     * of base-2 log of its resolution. For example, an encoder that can track 16 positions
+     * is a 4-bit encoder (2^4 = 16), thus 4 tracks. An incremental encoder will have 1 track
+     * at a minimum, and may have 1 index track and 1 quadrature track for a maximum of 3.
+     * 
+     * @return number of tracks in the encoder as Integer
+     */
+    public int getTrackCount() {
+        Integer result=1;
+        
+        if (this.type.equals(this.ABSOLUTE)) {
+            result = this.absoluteResolution.getValue();
+        } else if (this.type.equals(this.INCREMENTAL)) {
+            if (this.quadratureTrack.getValue()) result += 1;
+            if (this.indexTrack.getValue()) result += 1;
+        }
+        return result;
+    }
+
     private SimpleStringProperty type;
     private SimpleIntegerProperty resolution; // TODO use multiple resolutions?
+    private SimpleIntegerProperty absoluteResolution; // TODO use multiple resolutions?
+    private SimpleIntegerProperty incrementalResolution; // TODO use multiple resolutions?
     private SimpleIntegerProperty outerDiameter;
     private SimpleIntegerProperty innerDiameter;
     private SimpleIntegerProperty centerDiameter;
@@ -139,9 +191,9 @@ final public class Encoder {
 
     /* Generic type */
     public Encoder() {
-        unitOptions = Arrays.asList("mm", "in");
-        typeOptions = Arrays.asList("absolute", "incremental");
-        codingOptions = Arrays.asList("gray", "binary");
+        unitOptions = Arrays.asList(this.MM, this.INCH);
+        typeOptions = Arrays.asList(this.ABSOLUTE, this.INCREMENTAL);
+        codingOptions = Arrays.asList(this.GRAY, this.BINARY);
         
         outerDiameter = new SimpleIntegerProperty(50);
         innerDiameter = new SimpleIntegerProperty(20);
--- WheelEncoderGenerator/src/main/java/com/botthoughts/wheelencodergenerator/PrimaryController.java
+++ WheelEncoderGenerator/src/main/java/com/botthoughts/wheelencodergenerator/PrimaryController.java
@@ -17,13 +17,15 @@ package com.botthoughts.wheelencodergenerator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.beans.property.Property;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
@@ -31,7 +33,8 @@ import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
 import javafx.util.converter.IntegerStringConverter;
 
 
@@ -68,6 +71,111 @@ public class PrimaryController implements Initializable {
     @FXML
     CheckBox quadrature;
 
+    @FXML
+    Canvas canvas;
+    
+    private static int diameter = 500;
+    
+        /* getOffset()
+     *
+     * Returns the offset in degrees of the current track
+     */
+    private double getTrackOffset(int whichTrack)
+    {
+        double offset = 0.0;
+
+        // In all cases, offset is zero, except if the encoder is:
+        // 1) of type absolute and encoding is Gray Code, in which case, offset
+        // depends on which track we're talking about among other things
+        // 2) the current track is the quadrature track
+        // TODO: clockwise vs counter clockwise
+        /*
+        if (type == WheelEncoder.ABSOLUTE && numbering == WheelEncoder.GRAY) {
+            if (whichTrack == resolution-1)
+                offset = 0; //-getDegree(whichTrack, 0); // CCW: 0, CW: -getDegree(whichTrack, 0)
+            else
+                offset = -getDegree(whichTrack, 0)/2; // CW: - CCW: +
+        } else if (type == WheelEncoder.STANDARD) {
+            if (whichTrack == getQuadratureTrack()) {
+                offset = getDegree(whichTrack, 0)/2;
+            }
+        }
+        */
+        //Debug.println("track="+whichTrack);
+        //Debug.println("offset="+offset);
+        return offset;
+    }
+    
+    public void drawEncoder(GraphicsContext gc) {
+        double maxDiameter = Math.min(canvas.getWidth(), canvas.getHeight());
+        double centerX = canvas.getWidth() / 2.0;
+        double centerY = canvas.getHeight() / 2.0;
+        
+        Integer id = encoder.getInnerDiameter().getValue();
+        Integer od = encoder.getOuterDiameter().getValue();
+        Integer cd = encoder.getCenterDiameter().getValue();
+        
+        double ioRatio = (double) id / (double) od; // ratio of inner to outer diameter
+        double innerDiam = this.diameter * (double) id / (double) od;
+        double trackWidth = (this.diameter - innerDiam) / 2.0;
+        double ctrRatio = (double) cd / (double) od;
+        double ctrDiam = this.diameter * ctrRatio;
+        double ctrWidth = (this.diameter - ctrDiam) / 2.0;
+        double degree = 0;
+        double offset = 0;
+        int maxTrack = 0;
+        Color color;
+
+        // Background
+        /*
+        if (background != null) {
+            g2D.setColor(background);
+            g2D.fillRect(0, 0, (int) Math.round(width), (int) Math.round(height));
+        }*/
+
+        maxTrack = encoder.getTrackCount();
+
+        /*
+        for (int track = 0; track < maxTrack; track++) {
+            offset = e.getOffset(track);
+            double dA = innerDiam + (maxTrack-track) * (diameter - innerDiam - 1) / maxTrack;
+            double xA = x + track * trackWidth / maxTrack;
+            double yA = y + track * trackWidth / maxTrack;
+
+            if (e.isInverted())
+                color = Color.white;
+            else
+                color = Color.black;
+            int stripe = 0;
+            for (double i=offset; i < (360.0+offset); i += degree) {
+                degree = e.getDegree(track, stripe++);
+                g2D.setColor( color );
+                g2D.fill( new Arc2D.Double(xA, yA, dA, dA, i, degree, Arc2D.PIE) );
+                if (color == Color.white)
+                    color = Color.black;
+                else
+                    color = Color.white;
+            }
+            g2D.setColor(Color.black);
+            g2D.drawOval((int) Math.round(xA), (int) Math.round(yA), (int) Math.round(dA), (int) Math.round(dA));
+        }
+        */
+        
+        // Draw inner circle
+        gc.strokeOval(0, 0, maxDiameter, maxDiameter);
+        
+        /*
+        gc.setFill(Color.WHITE);
+        gc.fillOval((int) Math.round(x+trackWidth), (int) Math.round(y+trackWidth), (int) Math.round(innerDiam), (int) Math.round(innerDiam));
+        gc.setFill(Color.BLACK);
+        gc.drawOval((int) Math.round(x+trackWidth), (int) Math.round(y+trackWidth), (int) Math.round(innerDiam), (int) Math.round(innerDiam));
+        // Draw center circle
+        gc.drawOval((int) Math.round(x+ctrWidth), (int) Math.round(y+ctrWidth), (int) Math.round(ctrDiam), (int) Math.round(ctrDiam));
+        // Draw crosshairs
+        gc.drawLine((int) Math.round(x+trackWidth), (int) Math.round(y+diameter/2), (int) Math.round(x+(diameter+innerDiam)/2), (int) Math.round(y+diameter/2));
+        gc.drawLine((int) Math.round(x+diameter/2), (int) Math.round(y+trackWidth), (int) Math.round(x+diameter/2), (int) Math.round(y+(diameter+innerDiam)/2));        
+        */
+    }
     
     @FXML
     private void handleUnits(ActionEvent e) {
@@ -127,7 +235,7 @@ public class PrimaryController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {   
         
         encoder = new Encoder();
-        
+                
         outer.textProperty().bindBidirectional((Property) encoder.getOuterDiameter(), new IntegerStringConverter());
         inner.textProperty().bindBidirectional((Property) encoder.getInnerDiameter(), new IntegerStringConverter());
         center.textProperty().bindBidirectional((Property) encoder.getCenterDiameter(), new IntegerStringConverter());
@@ -151,6 +259,9 @@ public class PrimaryController implements Initializable {
         quadrature.selectedProperty().bindBidirectional((Property) encoder.getQuadratureTrack());
         index.selectedProperty().bindBidirectional((Property) encoder.getIndexTrack());
         incResolution.setValueFactory(new IntegerSpinnerValueFactory(2, 2048, 16, 1));
+        
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        this.drawEncoder(gc);
         
         // TODO draw circle
         // TODO print
